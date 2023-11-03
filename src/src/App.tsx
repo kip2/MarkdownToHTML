9f130de40734441809d63790c04ab364a696cd43
kip2 <tqa1no4@gmail.com>
2023-11-03 22:22:53 +0900

    editorの値の取得

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,10 +1,30 @@
 import './App.css'
 import Editor from "@monaco-editor/react"
+import { useRef } from 'react';
 
 function App() {
+  const editorRef = useRef("");
+
+  function handleEditorDidMount(editor) {
+    editorRef.current = editor;
+  }
+
+  function showValue() {
+    alert(editorRef.current.getValue());
+  }
 
   return (
-    <Editor height="90vh" width="90vw" defaultLanguage="javascript" defaultValue="// some comment" />
+    <>
+      <button onClick={showValue}>Show Value</button>
+      <Editor 
+        height="90vh" 
+        width="50vw" 
+        theme="vs-dark"
+        defaultLanguage="markdown" 
+        defaultValue="// some comment" 
+        onMount={handleEditorDidMount}
+      />
+    </>
   )
 }
 
