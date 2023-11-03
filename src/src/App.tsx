bac32a436774f237122080127222b7c96d393902
kip2 <tqa1no4@gmail.com>
2023-11-04 00:04:23 +0900

    fix: テンプレコメント変更

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -3,7 +3,7 @@ import MonacoPreview from './MonacoPreview'
 import styled from "styled-components";
 import "sanitize.css"
 import { useState } from 'react';
-import MakdownTransfer from './MarkdownTransfer';
+import MakdownPreview from './MarkdownPreview';
 
 
 const Wrapper = styled.div`
@@ -11,7 +11,20 @@ const Wrapper = styled.div`
 `;
 
 function App() {
-  const [editorValue, setEditorValue] = useState("//some comment");
+  const [editorValue, setEditorValue] = useState(`//some comment
+
+
+# Heading
+
+This is a *markdown* example.
+
+- List item 1
+- List item 2
+
+---
+
+[Recursion](https://recursionist.io/)
+  `);
 
   return (
     <>
@@ -20,7 +33,7 @@ function App() {
           editorValue={editorValue}
           setEditorValue={setEditorValue}
         />
-        <MakdownTransfer 
+        <MakdownPreview 
           editorValue={editorValue}
         />
       </Wrapper>
--- src/src/MarkdownPreview.tsx
+++ src/src/MarkdownPreview.tsx
@@ -2,23 +2,14 @@ import ReactMarkdown from "react-markdown";
 import styled from "styled-components";
 
   const Wrapper = styled.div`
+    margin-top:20px;
     height: 90vh;
     width: 50vw;
     text-align: left;
     padding-left:20px;
   `;
 
-const markdown = `# Heading
-
-This is a *markdown* example.
-
-- List item 1
-- List item 2
-
-[OpenAI](https://openai.com)
-`;
-
-export default function MakdownTransfer({ editorValue }) {
+export default function MakdownPreview({ editorValue }) {
     return (
         <Wrapper>
             {/* <ReactMarkdown children={markdown} /> */}
