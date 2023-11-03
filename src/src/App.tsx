6a98582a6a0c05cf6c72ab63a8079e463a62b60b
kip2 <tqa1no4@gmail.com>
2023-11-03 23:16:54 +0900

    add: リセットcss

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import MarkdownBuf from './MarkdownBuf'
 import MonacoPreview from './MonacoPreview'
 import styled from "styled-components";
+import "sanitize.css"
 
 const Wrapper = styled.div`
   display: flex;
