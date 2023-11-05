31bbdc8df50c0ec89ec63a5ce43fb7bba0a08c2a
kip2 <tqa1no4@gmail.com>
2023-11-05 13:34:21 +0900

    fix: HTMLPreviewに型定義を追加

--- src/src/HTMLPreview.tsx
+++ src/src/HTMLPreview.tsx
@@ -3,17 +3,22 @@ import styled from "styled-components";
 import remarkGfm from "remark-gfm";
 import jsxToHtml from "./jsxToHtml";
 
-  const Wrapper = styled.div`
-    height: 90vh;
-    width: 50vw;
-    text-align: left;
-    padding-left:20px;
-    border: solid 1px black;
-    background-color: white;
-    overflow-y: auto;
-  `;
+const Wrapper = styled.div`
+height: 90vh;
+  width: 50vw;
+  text-align: left;
+  padding-left:20px;
+  border: solid 1px black;
+  background-color: white;
+  overflow-y: auto;
+`;
 
-export default function HTMLPreview({ editorValue, isMarkdownHidden=true }) {
+type HTMLPreviewProps = {
+  editorValue: string 
+  isMarkdownHidden: boolean
+}
+
+export default function HTMLPreview({ editorValue, isMarkdownHidden=true }: HTMLPreviewProps) {
   const resultHtml = jsxToHtml(
             <ReactMarkdown 
               remarkPlugins={[remarkGfm]}
